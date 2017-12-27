7d345e0946380bd790796d335744d18b561d9bbd
이준호 <junho@ijunhoui-MacBook-Pro.local>
2017-12-27 10:27:58 +0900

    couldn't do the advanced

--- 01-rockPaperScissors/rockPaperScissors.js
+++ 01-rockPaperScissors/rockPaperScissors.js
@@ -17,7 +17,47 @@
 *
 */
 
-var rockPaperScissors = function (
-) {
-  // TODO: your solution here
+var rockPaperScissors = function () {
+  var list = ["rock", "paper", "scissors"];
+  var list_of_sequence = [];
+  var sequence = [];
+  for (var i = 0; i < 3; i++){
+    sequence.push(list[i]);
+    for (var k = 0; k < 3; k++){
+      sequence.push(list[k]);
+      for (var j = 0; j < 3; j++){
+        sequence.push(list[j]);
+        list_of_sequence.push(sequence);
+        sequence = sequence.slice(0, 2);
+      }
+      sequence = sequence.slice(0, 1);
+    }
+    sequence = sequence.slice(0, 0);
+  }
+  return list_of_sequence;
 };
+
+console.log(rockPaperScissors());
+
+// var rockPaperScissors = function (number) {
+//   var list = ["rock", "paper", "scissors"];
+//   var list_of_sequence = [];
+//   var sequence = [];
+//   var find_sequence = function(times){
+//     for (var i = 0; i < tiems; i++){
+//       if (sequence.length < time){
+//         sequence.push(list[i]);
+//         find_sequence(times);
+//         sequence = sequence.slice(0, sequence.length - 1)
+//       } else if (sequence.length = times) {
+//         list_of_sequence.push(sequence);
+//         sequence = sequence.slice(0, times - 1);
+//       }
+//     }
+//   }
+//   find_sequence(number);
+//
+//   return list_of_sequence;
+// };
+//
+// console.log(rockPaperScissors(3));

fc1d4b24fb6db6fc8c9b4331c19d10055f8e80aa
이준호 <junho@ijunhoui-MacBook-Pro.local>
2018-01-11 10:01:58 +0900

    11 done

---
+++ 11-rotatedArraySearch/rotatedArraySearch.js
@@ -0,0 +1,27 @@
+/*
+ * Given a sorted array that has been rotated some number of items right or
+ * left, i.e. [0, 1, 2, 3, 4, 5, 6, 7] might become [4, 5, 6, 7, 0, 1, 2, 3]
+ * how can you efficiently find an element? For simplicity, you can assume
+ * that there are no duplicate elements in the array.
+ *
+ * rotatedArraySearch should return the index of the element if it is in the
+ * array and should return null otherwise.
+ *
+ * For instance:
+ * rotatedArraySearch([4, 5, 6, 0, 1, 2, 3], 2) === 5
+ *
+ * rotatedArraySearch([4, 5, 6, 0, 1, 2, 3], 100) === null
+ *
+ * Target time complexity: O(log(array.length))
+ */
+
+var rotatedArraySearch = function (rotated, target) {
+  if (target > rotated.length){
+    return null
+  } else {
+    target > rotated[0] ? rotated.indexOf(target) : rotated.lastIndexOf(target)
+  }
+};
+
+// console.log(rotatedArraySearch([4, 5, 6, 0, 1, 2, 3], 2));
+// console.log(rotatedArraySearch([4, 5, 6, 0, 1, 2, 3], 100));

f44a1135670d74be724a2fa7654f16e76dd95eac
이준호 <junho@ijunhoui-MacBook-Pro.local>
2018-01-13 10:35:21 +0900

    13 done

--- 12-powerSet/powerSet.js
+++ 12-powerSet/powerSet.js
@@ -34,24 +34,81 @@ var powerSet = function(str){
     return outcome;
 }
 
-// console.log(powerSet('abc'));
+console.log(powerSet('abc'));
 
 
 // other solution
-var powerSet = function(str){
-    var outcome = [];
-    var recursive = (input, num) => {
-        for (var i = 0; i < str.length; i++){
-            var char = input.slice(i, num).split('').sort().join('');
-            if (!outcome.includes(char)) {
-                outcome.push(char)
-            }
-            if (num < input.length) {
-                recursive(input, num + 1)
-            }
-        }
+// var powerSet = function(str){
+//     var outcome = [];
+//     var recursive = (input, num) => {
+//         for (var i = 0; i < str.length; i++){
+//             var char = input.slice(i, num).split('').sort().join('');
+//             if (!outcome.includes(char)) {
+//                 outcome.push(char)
+//             }
+//             if (num < input.length) {
+//                 recursive(input, num + 1)
+//             }
+//         }
+//
+//     }
+//     recursive(str, 0);
+//     return outcome;
+// }
 
+
+// teacher solution
+//-----------------------------------------------------------------------------
+// Solution 1
+//-----------------------------------------------------------------------------
+var powerSet = function (str) {
+  var item = 0,
+      total = [],
+      destination = str.length - 1;
+  var iter = function (pre, starter) {
+    for (; total.push(item = pre.concat([str[starter]]).toString()) && starter < destination;) {
+      console.log('total : ', total)
+      iter(item, ++starter);
     }
-    recursive(str, 0);
-    return outcome;
-}
+  };
+  if (destination) {
+    iter([], 0);
+  }
+  return total;
+};
+
+//-----------------------------------------------------------------------------
+// Solution 2
+//-----------------------------------------------------------------------------
+var powerSet = function (str) {
+  var set = [];
+  var hash = {};
+  if (!str) str = '';
+  str = str.split('').sort();
+
+  for (var i = 1; i < str.length; i++) {
+    if (str[i - 1] === str[i]) {
+      str.splice(i, 1);
+      i--;
+    }
+  }
+
+  function recurse(strSet) {
+    var joined = strSet.join('');
+
+    if (hash[joined]) return;
+    hash[joined] = true;
+    set.push(joined);
+
+    if (strSet.length === 1) return;
+
+    for (var i = 0; i < strSet.length; i++) {
+      var subSet = strSet.slice(0, i).concat(strSet.slice(i + 1));
+      recurse(subSet);
+    }
+  }
+  recurse(str);
+  set.push('');
+
+  return set;
+};
